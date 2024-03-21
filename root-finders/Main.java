ecd4a150bec5939e74b63ba0b4924563614cf3a0
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2024-03-21 09:54:29 -0300

    Create Main.java

---
+++ root-finders/Main.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("bisection method:");
+        RootFinderMethods.bisectionMethod(0, 1, 10e-10);
+    }
+}
+
