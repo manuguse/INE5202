bb66c8da0ca6b1bf87d0adad8e8f8e16b31bfd24
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2024-03-25 10:02:04 -0300

    Update and rename eExpN.cpp to eExpN/Main.java

---
+++ eExpN/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("primeiro modo:");
+        System.out.println(eExpN.firstMode(2));
+        System.out.println("\nsegundo modo:");
+        System.out.println(eExpN.secondMode(2, 1e-16));
+    }
+}
